82b9c6d31cd86003838c755b9ff6d38c41b67abf
zelda2024 <161993446+zelda2024@users.noreply.github.com>
2025-01-25 23:25:52 +0800

    添加前端代码

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <body>
+        <Button type='primary'>测试antDesign</Button>
+      </body>
+    </div>
+  );
+}
+
+export default App;
